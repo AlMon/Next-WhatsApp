312e879d168d780bfcc2046a227a8c4e2818eda8
Al Mondragón <al.drago@live.com>
2021-04-10 20:33:41 -0500

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,14 @@
+function Sidebar() {
+  return (
+    <div>
+      <p className="text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        officia qui corrupti voluptatem, totam eveniet deserunt harum quisquam
+        optio laborum natus dolorem iusto quis distinctio voluptatum illum
+        maiores ipsa magnam.
+      </p>
+    </div>
+  )
+}
+
+export default Sidebar
